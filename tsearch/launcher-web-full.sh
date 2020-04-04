8be0789a6fdcac018edaf0f6ecd951c1ac1c0c27
gabo <gabriel.gaboardi13@gmail.com>
2020-04-04 13:41:22 -0300

    Improved the dmenu launcher, being now able to optionally choose the program and engine to search

--- tsearch/launcher-web-full.sh
+++ tsearch/launcher-web-full.sh
@@ -1,19 +1,41 @@
-#!/bin/sh
+#!/bin/bash
 
 TSEARCH=$HOME/Scripts/pytools/tsearch/tsearch.sh
 
+ENGINE_OPTION="[*] CHANGE ENGINE"
+PROGRAM_OPTION="[*] CHANGE PROGRAM"
 CLIP_OPTION=$(xclip -selection c -o)
 
-ENGINE_CHOICES="google\nduckduckgo\nyoutube"
-
+ENGINE_CHOICES="google\nduckduckgo\nyoutube\nnetflix\nwikipedia"
 PROGRAM_CHOICES="firefox\nqutebrowser\ngoogle-chrome-stable"
 
-PROGRAM=$(echo -e "$PROGRAM_CHOICES" | dmenu -p "Program:" -i)
+PROMPT="tsearch"
+COMMAND=$TSEARCH
+IS_SPECIAL_INPUT=1
 
-ENGINE=$(echo -e "$ENGINE_CHOICES" | dmenu -p "Engine:" -i)
+main(){
+    while [ $IS_SPECIAL_INPUT == 1 ]; do
+        TERM=$(echo -e "$CLIP_OPTION\n$ENGINE_OPTION\n$PROGRAM_OPTION" | dmenu -p "$PROMPT:" -i)
+        case "$TERM" in 
+            "$ENGINE_OPTION") 
+                ENGINE=$(echo -e "$ENGINE_CHOICES" | dmenu -p "Engine:" -i)
+                PROMPT="$ENGINE"
+                COMMAND="$COMMAND -e $ENGINE"
+                IS_SPECIAL_INPUT=1
+            ;;
+            "$PROGRAM_OPTION") 
+                PROGRAM=$(echo -e "$PROGRAM_CHOICES" | dmenu -p "Program:" -i)
+                COMMAND="$COMMAND -p $PROGRAM"
+                IS_SPECIAL_INPUT=1
+            ;;
+            *)
+                IS_SPECIAL_INPUT=0
+            ;;
+        esac
+    done
+    if [ -n "$TERM" ]; then
+        $COMMAND -t "$TERM"
+    fi
+}
 
-TERM=$(echo $CLIP_OPTION | dmenu -p "Search:" -i)
-
-if [ -n "$TERM" ]; then
-    $TSEARCH -p $PROGRAM -e $ENGINE -t "$TERM"
-fi
+main
