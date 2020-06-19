c1b11421c5c21f3214de547a53141334f976e1f3
gabo <gabriel.gaboardi13@gmail.com>
2020-06-18 21:29:54 -0300

    made sure that clipboard term doesn't break li as dmenu option

--- tsearch/tsearch-dmenu.sh
+++ tsearch/tsearch-dmenu.sh
@@ -7,7 +7,7 @@ CONFIG=$HOME/Scripts/pytools/tsearch/params.yaml
 
 ENGINE_OPTION=">> CHANGE ENGINE"
 PROGRAM_OPTION=">> CHANGE PROGRAM"
-CLIP_OPTION=$(xclip -selection c -o)
+CLIP_OPTION=$(echo $(xclip -selection c -o))
 
 ENGINE_CHOICES=$($parse $CONFIG -k engine)
 PROGRAM_CHOICES=$($parse $CONFIG -k program)
@@ -27,7 +27,7 @@ main(){
         TERM=$(echo -e "$CLIP_OPTION\n$ENGINE_OPTION\n$PROGRAM_OPTION" | dmenu -p "$PROMPT:" -i)
         case "$TERM" in 
             "$ENGINE_OPTION") 
-                SELECTED_ENGINE=$(echo -e "$ENGINE_CHOICES" | dmenu -p "Engine:" -i)
+                SELECTED_ENGINE=$(echo -e "$ENGINE_CHOICES" | dmenu -p "Engine:" -i -l 10)
                 if [ -n "$SELECTED_ENGINE" ]; then
                     ENGINE="$SELECTED_ENGINE"
                     PROMPT="$ENGINE"
